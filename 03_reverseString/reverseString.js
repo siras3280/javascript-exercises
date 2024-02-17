67128cf3c20fd8e83365c3d454676545da402ab8
Bryan Wekesa <bwekesa16@gmail.com>
2024-02-17 23:37:33 +0300

    Added a loop to reverse strings

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,14 @@
-const reverseString = function() {
-
+const reverseString = function(words) {
+    let wordsArray = words
+    let reversedWords = "";
+    for (i = 0; i < wordsArray.length; i++){
+        let value = i + 1;
+        if (value <= wordsArray.length){
+            reversedWords = wordsArray[i] + reversedWords;
+        }
+        
+    }
+    return reversedWords;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
