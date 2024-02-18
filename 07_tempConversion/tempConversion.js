d4244bd98b5051336f7ed7863bdadb6c52b7708c
Bryan Wekesa <bwekesa16@gmail.com>
2024-02-18 19:50:49 +0300

    Added formulas that enable the conversions and rounded results

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(tempFahrenheit) {
+  const subtractor = 32;
+  const multiplier = 5 / 9;
+  let result = ((tempFahrenheit - subtractor) * multiplier);
+  let decimalPlaces = Math.pow(10,1);
+  return Math.round(result * decimalPlaces)/decimalPlaces ;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(tempCelsius) {
+  const multiplier = 9 / 5;
+  const additive = 32;
+  let result = ((tempCelsius * multiplier) + additive)
+  let decimalPlaces = Math.pow(10,1);
+  return Math.round(result * decimalPlaces)/decimalPlaces ;
+  
 };
 
 // Do not edit below this line
