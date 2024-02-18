4352614c6470f2dddcf8c9e20d25d8508e0f9be3
Bryan Wekesa <bwekesa16@gmail.com>
2024-02-18 19:18:09 +0300

    added conditionals and used AP formula

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
+const sumAll = function(valueA,valueB) {
+    if (valueA < 0 || valueB < 0 || typeof valueA != 'number' || typeof valueB != "number"){
+        return "ERROR";
+    };
+
+    let smallerNumber;
+    let largerNumber;
+    if (valueA > valueB){
+        smallerNumber = valueB;
+        largerNumber = valueA;
+    }
+    else{
+        smallerNumber = valueA;
+        largerNumber = valueB;
+    }
+    let numberOfTerms = (largerNumber - smallerNumber) + 1;
+    let commonDifference = 1;
+    let sum ;
+    sum = (numberOfTerms / 2) * ((2 * smallerNumber) + ((numberOfTerms - 1) * commonDifference))
+    return sum;
 
 };
-
+console.log(sumAll(5,[1,2]))
 // Do not edit below this line
 module.exports = sumAll;
