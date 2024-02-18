9411068f409a80543db4cad1fd455c9a393af75b
Bryan Wekesa <bwekesa16@gmail.com>
2024-02-18 18:46:01 +0300

    Added for loop to check if similar and conditional to check duplicates

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,41 @@
-const removeFromArray = function() {
+const removeFromArray = function(array,...removedItems) {
+    let remove = [].slice.call(removedItems);
+    
+    let newArray = [];
+    for (i = 0; i < array.length; i++){
+        
+        for(j = 0; j < remove.length; j++){
+            if (array[i] != remove[j]){
+                if (newArray[i] != remove[j]){
+                    newArray.push(array[i]);
+                   
+                }
+                
+            }
+        }
+       
+    }
+    for(j = 0; j < (newArray.length);j++){
+        if(newArray.includes(remove[j])== true){
+            newArray = newArray.filter(function(item){
+                return item !== remove[j];
+            })
+        }
+       
+    }
 
+
+      let newArray1 = [];
+      if (new Set(newArray).size ==1 ){
+        newArray1 = [];
+      }
+      else{
+        newArray1 =  Array.from(new Set(newArray))
+      }
+    
+   
+    return newArray1;
 };
-
+console.log(removeFromArray([1,2,3,4,5,6],3,4,5,6))
 // Do not edit below this line
 module.exports = removeFromArray;
